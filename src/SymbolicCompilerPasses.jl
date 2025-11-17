b9a4caa5774bc781acc9f38b3b7fb9a86a9ad896
Dhairya Gandhi <dhairya@juliahub.com>
2025-11-17 14:41:17 +0000

    chore: imports

--- src/SymbolicCompilerPasses.jl
+++ src/SymbolicCompilerPasses.jl
@@ -6,7 +6,9 @@ using SymbolicUtils
 import SymbolicUtils: symtype, vartype, Sym, BasicSymbolic, Term, iscall, operation, arguments, maketerm, Const
 import SymbolicUtils.Code: Code, OptimizationRule, substitute_in_ir, apply_optimization_rules, AbstractMatched,
     Assignment, CSEState, lhs, rhs, apply_substitution_map, bank
+using StaticArrays
 
 include("matmuladd.jl")
+include("hvncat_static_opt.jl")
 
 end # module SymbolicCompilerPasses
