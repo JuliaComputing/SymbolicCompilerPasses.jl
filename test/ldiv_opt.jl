8c09ffcb5c1e9b465018b576ae0711e158249ec8
Dhairya Gandhi <dhairya@juliahub.com>
2025-11-20 16:06:34 +0000

    test(ldiv): add other test example

--- test/ldiv_opt.jl
+++ test/ldiv_opt.jl
@@ -6,7 +6,6 @@ using LinearAlgebra
 using Test
 
 function test_ldiv(expr, args...)
-
     current = SU.Code.cse(expr)
     optimized = SC.ldiv_opt(current, SU.Code.CSEState())
 
@@ -20,18 +19,24 @@ function test_ldiv(expr, args...)
     N = 16
     a = randn(M, M)
     b = randn(M, N)
+    c = randn(M, N)
 
-    current_o = @invokelatest current_f(a, b)
-    optimized_o = @invokelatest optimized_f(a, b)
+    if length(args) == 2
+        current_o = @invokelatest current_f(a, b)
+        optimized_o = @invokelatest optimized_f(a, b)
+    elseif length(args) == 3
+        current_o = @invokelatest current_f(a, b, c)
+        optimized_o = @invokelatest optimized_f(a, b, c)
+    end
     @test isapprox(current_o, optimized_o, rtol=1e-10, atol=1e-10)
 end
 
 @testset "LDiv Factorization" begin
-    @syms A[1:3, 1:3] B[1:3, 1:2] C[1:3, 1:3]
+    @syms A[1:3, 1:3] B[1:3, 1:2] C[1:3, 1:2]
 
     expr = A \ B
     test_ldiv(expr, A, B)
 
-    # expr2 = C + (A \ B)
-    # @test test_ldiv(expr2, A, B, C)
+    expr2 = C + (A \ B)
+    test_ldiv(expr2, A, B, C)
 end
