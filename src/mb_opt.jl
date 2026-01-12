c905f158391c998f39cfabbf582c976624096287
Dhairya Gandhi <dhairya@juliahub.com>
2026-01-12 13:02:14 +0000

    chore: convert small views to static arrays

---
+++ src/mb_opt.jl
@@ -0,0 +1,59 @@
+function detect_small_views(expr::Code.Let, state)
+    matches = []
+    for (i, p) in enumerate(expr.pairs)
+        r = rhs(p)
+        iscall(r) || continue
+        if operation(r) === view
+            push!(matches, (idx = i, expr = r))
+        end
+    end
+    matches
+end
+
+function construct_type(dims)
+    # if length(dims) == 1
+    #     return Core.apply_type(SVector, dims[1])
+    # else
+    #     return Core.apply_type(SVector, Tuple(dims))
+    # end
+    Core.apply_type(SVector, length(dims))
+end
+
+function transform_view(expr, match_data, state)
+    new_pairs = []
+    idxs = Set(getproperty.(match_data, :idx))
+    transformations = Dict()
+    for match in match_data
+        idx = match.idx
+        r = match.expr
+        T = symtype(r)
+        V = vartype(r)
+        arr, inds... = arguments(r)
+        @show length.(inds)
+        t = @show term(construct_type, inds[1])
+        # transformations[idx] = term(Core.apply_type(SVector, size(inds[1])), r)
+        # @show Term{V}(t, [r], type = T)
+        # transformations[idx] = term(term(construct_type, inds[1]), r)
+        transformations[idx] = Term{V}(t, [r], type = T)
+    end
+
+    # @show transformations[3]
+    for (i, p) in enumerate(expr.pairs)
+        if i in idxs
+            new_rhs = transformations[i]
+            push!(new_pairs, Code.Assignment(lhs(p), new_rhs))
+        else
+            push!(new_pairs, p)
+        end
+    end
+
+    Code.Let(new_pairs, expr.body, expr.let_block)
+end
+
+
+const MB_VIEW_RULE = OptimizationRule(
+    "MB_VIEW_RULE",
+    detect_small_views,
+    transform_view,
+    10,
+)
